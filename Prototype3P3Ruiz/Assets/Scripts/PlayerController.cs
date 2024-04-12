b0d5ba5c1bb895c2a4fa2c6eb6184d6c71cf10d1
MiaR730 <723420@student.dusd.net>
2024-04-12 12:17:17 -0700

    Player Controller scrpit. scene and player.

---
+++ Prototype3P3Ruiz/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float jumpForce;
+    public float gravitymodifier;
+    public bool isOnGround = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravitymodifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            isOnGround = true;
+        }
+}
+}
