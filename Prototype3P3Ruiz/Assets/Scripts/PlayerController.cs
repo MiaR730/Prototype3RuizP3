367025abfe50c3e0c6a7b254a2ea54ffa008da13
MiaR730 <723420@student.dusd.net>
2024-04-16 11:40:46 -0700

    Finished 3.1 started 3.2

--- Prototype3P3Ruiz/Assets/Scripts/PlayerController.cs
+++ Prototype3P3Ruiz/Assets/Scripts/PlayerController.cs
@@ -7,29 +7,37 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
-    public float jumpForce;
-    public float gravitymodifier;
+    public float jumpForce = 10;
+    public float gravityModifier;
     public bool isOnGround = true;
+    public bool gameOver = false;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
-        Physics.gravity *= gravitymodifier;
+        Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
         }
-
+    }
         private void OnCollisionEnter(Collision collision)
         {
-            isOnGround = true;
+            if(collision.gameObject.CompareTag("Obstacle"))
+            {
+                isOnGround = true;
+            }
+            else if(collision.gameObject.CompareTag("Obstacle"))
+            {
+                Debug.Log("Game Over");
+                gameOver = true;
+            }
         }
-}
 }
