609b05b235f1d775636154ab4fbdc93b0bd94e5d
MiaR730 <723420@student.dusd.net>
2024-04-22 11:04:38 -0700

    Added sound and effects. finished project.
    Added effect to the player running and when gets hot added an explosion. added sound to the explosion and sound to player jump.

--- Prototype3P3Ruiz/Assets/Scripts/SpawnManager.cs
+++ Prototype3P3Ruiz/Assets/Scripts/SpawnManager.cs
@@ -9,6 +9,7 @@ public class SpawnManager : MonoBehaviour
     private float startDelay = 2;
     private float repeatRate = 2;
     private PlayerController playerControllerScript;
+    public GameObject[] obsticlePrefabs;
 
     // Start is called before the first frame update
     void Start()
